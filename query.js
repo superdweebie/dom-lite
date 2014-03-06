77b76acec34d30530ef39003bb9134b245ae0e7d
Tim Roediger <superdweebie@gmail.com>
2014-03-06 12:26:08 +1000

    Add ability to handle more css selectors

--- query.js
+++ query.js
@@ -9,16 +9,23 @@ var tokenize = function(queryString){
         if (chr == ' '){
             tokens.push(token);
             delete(token);
+        } else if (chr == '.'){
+            if (token) tokens.push(token);
+            token = {type: 'class', name: ''};
         } else if (chr == '['){
             if (token) tokens.push(token);
             token = {type: 'attribute', name: ''};
         } else if (chr == ']'){
             tokens.push(token);
             token = null;
+        } else if (token && token.type == 'attribute' && (chr == '=' || chr == '*' || chr == '^' || chr == '$')){
+            token.operator += chr;
         } else if (!token){
             token = {type: 'tag', name: chr};
+        } else if (token && token.type == 'attribute' && token.operator){
+            token.compare += chr;
         } else {
-            token.name = token.name + chr;
+            token.name += chr;
         }
     }
 
@@ -29,21 +36,45 @@ var tokenize = function(queryString){
 
 exports.getTestFunc = function(queryString){
 
-    var tokens = tokenize(queryString);
+    var selectors = queryString.split(',').map(function(item){return item.trim()}).map(function(item){return tokenize(item)});
 
     return function(node){
-        var i,
-            token;
 
-        for (i = 0; i < tokens.length; i++){
-            token = tokens[i];
-            if (token.type == 'tag'){
-                if (node.tagName != token.name) return false;
-            } else if (token.type == 'attribute'){
-                if (!node.hasAttribute(token.name)) return false;
+        var i;
+        var j;
+        var classes = [];
+        var tokens;
+        var token;
+        var match;
+
+        if (node.hasAttribute('class')) classes = node.getAttribute('class').split(' ')
+
+        for (i = 0; i < selectors.length; i++){
+            match = true;
+            tokens = selectors[i];
+            for (j = 0; j < tokens.length; j++){
+                token = tokens[j];
+                if (token.type == 'tag'){
+                    if (node.tagName != token.name) match = false; break;
+                } else if (token.type == 'attribute'){
+                    if (!node.hasAttribute(token.name)) match = false; break;
+                    if (token.operator == '='){
+                        if (node.getAttribute(token.name) != token.compare) match = false; break;
+                    } else if (token.operator == '*='){
+                        if (node.getAttribute(token.name).indexOf(token.compare) == -1) match = false; break;
+                    } else if (token.operator == '^='){
+                        if (node.getAttribute(token.name).indexOf(token.compare) != 0) match = false; break;
+                    } else if (token.operator == '$='){
+                        var value = node.getAttribute(token.name);
+                        if (value.indexOf(token.compare) == value.length - token.compare.length) match = false; break;
+                    }
+                } else if (token.type == 'class'){
+                    if (classes.indexOf(token.name) == -1) match = false; break;
+                }
             }
+            if (match) return true
         }
 
-        return true;
+        return false;
     }
 };
